9c951d85cfe08c051124482ec35e088ad494b845
Alan <alanlichen1@gmail.com>
2024-11-20 20:53:57 -0800

    Ignore server directory with NextJS building

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,18 @@
 const nextConfig = {
     experimental: {
         optimizePackageImports: ['@chakra-ui/react'],
-    }
+    },
+    // ignore 'server' directory
+    webpack: (config) => {
+        if (process.env.NEXT_OUTPUT_MODE !== "export" || !config.module) {
+            return config;
+        }
+        config.module.rules?.push({
+            test: /server/,
+            loader: "ignore-loader",
+        });
+        return config;
+    },
 };
 
 export default nextConfig;
