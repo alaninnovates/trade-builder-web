5240db32a22325ef72f4b5f187bea5a0441b4d75
Alan <alanlichen1@gmail.com>
2024-11-20 21:27:20 -0800

    Rename SOCKET_URL env variable

--- app/lib/socket.ts
+++ app/lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001';
+const URL = process.env.SOCKET_URL || 'http://localhost:3001';
 
 export const socket = io(URL);
